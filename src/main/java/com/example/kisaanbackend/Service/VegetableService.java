76afe1584909451121acda2ded3d7498858aeff5
Ashwani5-deloitte <ashwanayak@deloitte.com>
2022-05-06 12:40:59 +0530

    added get all vegetable and by id API

--- src/main/java/com/example/kisaanbackend/Service/VegetableService.java
+++ src/main/java/com/example/kisaanbackend/Service/VegetableService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class VegetableService {
@@ -21,5 +22,15 @@ public class VegetableService {
 
     public List<Vegetable> getAllVegetables(){
         return vegetableRepository.findAll();
+    }
+
+    public Vegetable getVegetableById(int id){
+        Optional<Vegetable> optionalVegetable = vegetableRepository.findById(id);
+
+        if(optionalVegetable.isPresent()){
+
+            return optionalVegetable.get();
+
+        }else return  null;
     }
 }
